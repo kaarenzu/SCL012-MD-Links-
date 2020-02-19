5143bbfcbd5a63b2d29782a987e8a1cfba11cb39
kaarenzu <karenzcortes0211@gmail.com>
2020-02-19 18:56:00 -0300

    Cambios en el readme

--- readLinks.js
+++ readLinks.js
@@ -1,8 +1,2 @@
 let fs = require('fs');
 let markdownLink = require('markdown-link-extractor');
-
-const markdown = fs.readFileSync('ReadmeDos.md').toString();
-const links = markdownLink(markdown);
-links.forEach(function(link){
-  console.log(link);
-})
