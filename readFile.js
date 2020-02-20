0b9fb4bedd6e34c937fbff20b50116aec172efa0
kaarenzu <karenzcortes0211@gmail.com>
2020-02-20 13:26:51 -0300

    Cambios en la funcion readfile y readLink

--- readFile.js
+++ readFile.js
@@ -5,17 +5,17 @@ const markdownLink = require('markdown-link-extractor');
 const pathAbsolute = path.resolve(processArgv);
 
 
-exports.readFile = (pathAbsolute) => {
+const linkFile = (pathAbsolute) => {
   return new Promise((resolve, reject) => {
     fs.readFile(pathAbsolute, 'utf8', (error, data) => {
       if (error) return reject(error);
-      return resolve(data);
+      return resolve(markdownLink(data));
     })
   })
 } 
 
-exports.readFile(pathAbsolute)
-  .then(data => console.log(markdownLink(data)))
-  .catch(error => console.log('aca hay un fail ' + error));
+linkFile(pathAbsolute)
+  .then(data => console.log(data))
+  .catch(error => console.log(error));
 
 
--- readLinks.js
+++ readLinks.js
@@ -1,4 +1,48 @@
-let fs = require('fs');
-let markdownLink = require('markdown-link-extractor');
-let chalk = require('chalk');
+const fs = require('fs');
+const markdownLink = require('markdown-link-extractor');
+const chalk = require('chalk');
+const fetch = require ('node-fetch')
 console.log(chalk.blue('hola soy azul'))
+var fetchUrl = require("fetch").fetchUrl;
+
+
+// fetchUrl("http://kreata.ee/iso-8859-15.php",(error, meta, body) =>{
+//     console.log(meta.status);
+// });
+
+const urls = ['https://www.google.com', 'https://bing.com', 'https://www.npmjs.com/']
+const getData = (url) => {
+  return new Promise((resolve, reject) =>{
+    fetchUrl(url, (error, meta, body) =>{
+      if(meta){
+        console.log("el estado del sitio", url, "es: ", meta.status);
+        resolve(meta.status);
+      }
+    })
+  })
+}
+// const getStatusFromUrlArrays = (urlArray) => {
+//   console.log('hola')
+//   let statusCounter = 0;
+//   for(let i= 0; i< urlArray.length; i++){
+//     console.log("getStatuaFromUrlArrays url:", urlArray[i]);
+//     //llamamos a la función de arriba getData
+//     getData(urlArray[i])
+//     //llamamos al resultado que en la promesa de arriba sería el meta.status
+//     .then(res => {
+//       if(res === 200) {
+//         statusCounter += 1;
+//       }else{
+//         console.log("este no es 200")
+//       }
+//     })
+//   }
+//   console.log('statusCounter', statusCounter);
+// }
+// //numero 1
+ //getStatusFromUrlArrays(urls);
+//le paso el array de links y con el map por cada url que posee se lo pasamos a la funcion getData, si no le pasamos el error, lo hace con cada url del array. Después que termina llamamos al then y le pasamos estos resultados
+Promise.all(urls.map(url => getData(url).catch(err => 'broken')))
+  .then(results => console.log("Promise.all => results.length:", results.length));
+
+  
